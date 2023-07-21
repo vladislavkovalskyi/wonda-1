5ac41d612be3cb0690c14d4ea5510d2d7bd3dc4b
Vladislav Kovalskyi <69077401+vladislavkovalskyi@users.noreply.github.com>
2023-07-21 20:28:43 +0300

    Update token_util.py
    Made it possible to parse an .env file from the current project directory where the file is located. Because not all people store .env files in /home/ directory

--- wonda/api/utils/token_util.py
+++ wonda/api/utils/token_util.py
@@ -1,13 +1,18 @@
-from os import getenv
+from envparse import env
+import os
 from typing import NoReturn, Optional, Union
 
 
 class Token(str):
     @classmethod
     def from_env(
-        cls, var_name: Optional[str] = "WONDA_TOKEN"
+        cls,
+        file_name: Optional[str] = ".env",
+        var_name: Optional[str] = "WONDA_TOKEN"
     ) -> Union["Token", NoReturn]:
-        if token := getenv(var_name):
-            return cls(token)
+        env_file_path = os.path.join(os.path.abspath(os.curdir), file_name)
+        env.read_envfile(env_file_path)
 
-        raise KeyError(f"Environment variable {var_name!r} not found")
+        if not (token := env.str(var_name)):
+            raise KeyError(f"Environment variable {var_name!r} not found!")
+        return token
